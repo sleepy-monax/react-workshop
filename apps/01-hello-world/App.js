5343232435ce338653f6048b4aaa34b4ce9e6c0c
VAN BOSSUYT Nicolas <nvanbossuyt@underside.be>
2022-06-03 11:40:46 +0200

    More exemples.

--- apps/01-hello-world/App.js
+++ apps/01-hello-world/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, world!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,7 +18,3 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
-
-function deepCopy(obj) {
-  return JSON.parse(JSON.stringify(obj));
-}
